af968ad6fb4c951d57aa3014c5ee481c9e298333
Michael J. Radwin <michael@radwin.org>
2020-06-16 09:01:59 -0700

    Redo TypeScript definitions to be empty file

--- cities.d.ts
+++ cities.d.ts
@@ -3,18 +3,4 @@
 import {Location} from '@hebcal/core';
 
 declare module '@hebcal/cities' {
-    /**
-     * Interface to lookup cities
-     */
-    export namespace cities {
-        /**
-         * Looks up a city
-         * @param str - city name (such as "San Francisco" or "Jerusalem")
-         */
-        function lookup(str: string): Location;
-        /**
-         * Must be called before `getCity()`
-         */
-        function init(): void;
-    }
 }
